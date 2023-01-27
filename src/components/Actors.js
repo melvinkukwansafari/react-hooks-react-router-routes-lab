2267f7beb936446b9abdd340177fd40f105fb6b9
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-27 11:00:26 +0300

    some test passed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.name}>
+          <p>{actor.name}</p>
+          <ul>
+            {actor.movies.map(movie => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>);
 }
 
 export default Actors;
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes,  } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,21 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  
+ <div>
+     <NavBar />
+     <Routes>
+      
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/actors" component={Actors} />
+        <Route component={Home} />
+        </Routes>
+  </div>
+  );
+  
 }
 
 export default App;

d5d3a48f8de9126b99ccd00c781c3cb053e1c434
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-28 14:21:11 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes,  } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,21 +7,26 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return (
-  
- <div>
-     <NavBar />
-     <Routes>
-      
-        <Route exact path="/" component={Home} />
-        <Route exact path="/movies" component={Movies} />
-        <Route exact path="/directors" component={Directors} />
-        <Route exact path="/actors" component={Actors} />
-        <Route component={Home} />
-        </Routes>
-  </div>
-  );
-  
+
+
+
+  return <div>
+  <NavBar />
+  <Switch>
+    <Route path="/movies">
+      <Movies />
+    </Route>
+    <Route path="/directors">
+      <Directors />
+    </Route>
+    <Route path="/actors">
+      <Actors />
+    </Route>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
+  </div>;
 }
 
 export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return( <div>
-    <h1>Movies Page</h1>
-    {
-      movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <p>Time: {movie.time}</p>
-          <ul>
-            {movie.genres.map((genre) => (
-              <li key={genre}>{genre}</li>
-            ))}
-          </ul>
-        </div>
+const movieItems = movies.map((movie) => (
+  <div key={movie.title}>
+    <h2>{movie.title}</h2>
+    <p>Runtime: {movie.time} min.</p>
+    <ul>
+      {movie.genres.map((genre) => (
+        <li key={genre}>{genre}</li>
       ))}
-    </div>
-  );
-    
+    </ul>
+  </div>
+));
+
+function Movies() {
+  return <div>
+    <h1>Movies Page</h1>
+    {movieItems}
+  </div>;
 }
 
 export default Movies;
